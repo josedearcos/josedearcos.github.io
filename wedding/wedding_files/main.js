30b9b2ababccf2f7753ac411ec191e1d20001f43
JdeArcos <jf.dearcos@gmail.com>
2018-11-28 03:31:44 +0100

    solved mistery of images disappearing. High reso images are @2x, need normal

--- wedding/wedding_files/main.js
+++ wedding/wedding_files/main.js
@@ -1,101 +1,3 @@
-var animationMethods = {
-  slideInFromTheSide: function(callback) {
-    return function() {
-      if ($(this).hasClass('right')) {
-        $(this).animate({"margin-right": '0px'}, 500, callback);
-      } else if ($(this).hasClass('left')) {
-        $(this).animate({"margin-left": '0px'}, 500, callback);
-      }
-    }
-  },
-  slideBackToTheSide: function(callback) {
-    return function() {
-      if ($(this).hasClass('right')) {
-        $(this).animate({"margin-right": '-1800px'}, 500, callback);
-      } else if ($(this).hasClass('left')) {
-        $(this).animate({"margin-left": '-1800px'}, 500, callback);
-      }
-    }
-  },
-  selectNavItem: function() {
-    $("nav li").removeClass("selected");
-    $("nav a[href=#"+$(this).attr('id')+"]").parent().addClass("selected");
-  }
-}
-
-var WhereZone = function($element) {
-  this.moveIn = function() {
-    $element.each(animationMethods.selectNavItem);
-  }
-
-  this.moveOut = function() {};
-}
-
-var VenueZone = function($element) {
-  var $bird = $element.find("#bird");
-  var $text = $element.find("p");
-  var that = this;
-
-  this.moveIn = function() {
-    /*$.each($text, animationMethods.slideInFromTheSide());*/
-    $.each($bird, animationMethods.slideInFromTheSide());
-
-    $element.each(animationMethods.selectNavItem);
-  }
-
-  this.moveOut = function() {
-    /*$.each($text, animationMethods.slideBackToTheSide());*/
-    $.each($bird, animationMethods.slideBackToTheSide());
-
-  }
-}
-
-var EventiZone = function($element) {
-  var that = this;
-  var $bird = $element.find("#bird");
-  var $text = $element.find("p");
-
-  this.moveIn = function() {
-    /*$.each($text, animationMethods.slideInFromTheSide());*/
-    $.each($bird, animationMethods.slideInFromTheSide());
-    $element.each(animationMethods.selectNavItem);
-  }
-
-  this.moveOut = function() {
-    /*$.each($text, animationMethods.slideBackToTheSide());*/
-    $.each($bird, animationMethods.slideBackToTheSide());
-  }
-}
-
-var HotelsZone = function($element) {
-  var that = this;
-  var $text = $element.find("p");
-
-  this.moveIn = function() {
-    /*$text.each(animationMethods.slideInFromTheSide());*/
-    $element.each(animationMethods.selectNavItem);
-  }
-
-  this.moveOut = function() {
-    /*$right_cloud.each(animationMethods.slideBackToTheSide());*/
-    /*$text.each(animationMethods.slideBackToTheSide());*/
-  }
-}
-
-var FormZone = function($element) {
-  var that = this;
-  var $text = $element.find("p");
-
-  this.moveIn = function() {
-    /*$text.each(animationMethods.slideInFromTheSide());*/
-    $element.each(animationMethods.selectNavItem);
-  }
-
-  this.moveOut = function() {
-   /* $text.each(animationMethods.slideBackToTheSide());*/
-  }
-}
-
 $(document).ready(function() {
   var RAISE_THRESHOLD = 950;
   var DESCENT_THRESHOLD = 240;
@@ -117,31 +19,6 @@ $(document).ready(function() {
     {top: "3750", offset: "25%", css_class: "cloud-6", delay: .7},
     {top: "4050", offset: "-380px", css_class: "cloud-5", delay: 1.2},
   ];
-
-  var zoneControllers = {
-    "zone-where" : new WhereZone($("#zone-where")),
-    "zone-venue" : new VenueZone($("#zone-venue")),
-    "zone-eventi" : new EventiZone($("#zone-eventi")),
-    "zone-hotels" : new HotelsZone($("#zone-hotels")),
-    "zone-form" : new FormZone($("#zone-form")),
-  }
-
-  $.each(zoneControllers, function(index, controller) {
-    controller.moveOut();
-  });
-
-  var zones = [];
-
-  $(".moment").each(function(index, moment) {
-    var offset = $(moment).offset();
-    zones.push({
-      start: offset.top,
-      end: offset.top + $(moment).height(),
-      moment: $(moment),
-      controller: zoneControllers[moment.id],
-      name: $(moment).find("strong").text()
-    });
-  });
 
   function createClouds() {
     var cloudIndex = 0;
@@ -186,19 +63,6 @@ $(document).ready(function() {
 
     var edgePoint = isMovingUp ? flagOffset + RAISE_THRESHOLD : basketOffset - DESCENT_THRESHOLD;
 
-    $.each(zones, function(index, zone) {
-      if ((!activeZone || activeZone != zone) && edgePoint >= zone.start && edgePoint <= zone.end) {
-        if (activeZone && activeZone.controller) {
-          activeZone.controller.moveOut();
-        }
-        activeZone = zone;
-        if (activeZone && activeZone.controller) {
-          activeZone.controller.moveIn();
-        }
-        return false;
-      }
-    });
-
     if (atArrival && isArriving) {
       atArrival = false;
       $balloon.removeClass('soaring').addClass('rocking');
@@ -224,26 +88,8 @@ $(document).ready(function() {
     $(window).scroll(animationHandler);
   }
 
-  function setUpNavigation() {
-    $("nav li").click(function(e) {
-      var $moment = $($(this).find('a').attr('href'));
-      var balloonMarginTop = parseInt($balloon.css('margin-top'))
-      var hiddenBalloonHeight = balloonMarginTop < 0 ? -1 * balloonMarginTop : 0;
-
-      $moment.each(animationMethods.selectNavItem);
-
-      $('html, body').animate({
-        scrollTop: $moment.offset().top - $moment.height() / 2 + hiddenBalloonHeight - 100
-      }, 1000);
-
-      e.preventDefault();
-      return false;
-    })
-  }
-
   createClouds();
   setUpAnimations();
-  setUpNavigation();
 
   setTimeout(function() {
     $(window).trigger('scroll')
